827e5b6f8f0cde665fef66dcd9154e2d572d2e9c
lpattis1 <lpattis1@asu.edu>
2021-11-27 19:55:58 -0600

    testing.

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+var nodeExternals = require("webpack-node-externals");
+
+var mode = process.env.NODE_ENV || "development";
 
 const serverConfig = {
-	mode: process.env.NODE_ENV || 'development',
-	entry: './src/server/server.ts',
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {
-					configFile: 'tsconfig.server.json'
-				}
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-	output: {
-		filename: 'server.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	target: 'node',
-	node: {
-		__dirname: false
-	},
-	externals: [nodeExternals()]
+  mode,
+  entry: "./src/server/server.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.server.json",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "server.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  target: "node",
+  node: {
+    __dirname: false,
+  },
+  externals: [nodeExternals()],
 };
 
 const clientConfig = {
-	mode: process.env.NODE_ENV || 'development',
-	entry: './src/client/index.tsx',
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {
-					configFile: 'tsconfig.client.json'
-				}
-			},
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js', '.css', '.scss']
-	},
-	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, 'public/js')
-	}
+  mode,
+  entry: "./src/client/index.tsx",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.client.json",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".css", ".scss"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "public/js"),
+  },
 };
 
 module.exports = [serverConfig, clientConfig];
